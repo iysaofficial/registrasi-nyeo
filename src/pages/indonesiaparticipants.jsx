95869a9a5b4ae3da3081d5b75b5830588444daaa
iysaofficial <youngscientist.iysa@gmail.com>
2024-09-10 10:09:07 +0700

    update bug

--- src/pages/indonesiaparticipants.jsx
+++ src/pages/indonesiaparticipants.jsx
@@ -179,33 +179,45 @@ function IndoensiaParticipants() {
   useEffect(() => {
     const scriptURL =
       "https://script.google.com/macros/s/AKfycbwLe2jTE_3gTCzNbEWayXKWWLA-ljRWjEg68eKHoMp0AkqDWD4tK7hZfJ4W3l21lwL0/exec";
-
+  
     const form = document.forms["regist-form"];
-
+    var buttonCounter = 0;
     if (form) {
       const handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-          await fetch(scriptURL, { method: "POST", body: new FormData(form) });
+  
+        // Pengecekan paymentUrl di dalam handleSubmit
+        if (!paymentUrl) {
+          alert("Invoice berhasil di buat!! silahkan tekan tombol 'KIRIM' di akhir formulir pendaftaran");
+          return; // Menghentikan pengiriman jika URL pembayaran belum ada
+        }
 
-          // Setelah berhasil mengirim data, arahkan pengguna ke halaman lain
-          window.location.href = "/"; // Gantikan dengan URL halaman sukses Anda
-        } catch (error) {
-          console.error("Error saat mengirim data:", error);
-          // Handle error jika diperlukan
+        if (buttonCounter == 0) {
+          try {
+            buttonCounter++;
+            await fetch(scriptURL, { method: "POST", body: new FormData(form) });
+    
+            // Setelah berhasil mengirim data, arahkan pengguna ke halaman lain
+            window.location.href = "/"; // Gantikan dengan URL halaman sukses Anda
+          } catch (error) {
+            console.error("Error saat mengirim data:", error);
+            // Handle error jika diperlukan
+          }
         }
 
         form.reset();
       };
-
+  
       form.addEventListener("submit", handleSubmit);
-
+      
       // Membersihkan event listener saat komponen dilepas
       return () => {
         form.removeEventListener("submit", handleSubmit);
       };
+
+
     }
-  }, []);
+  }, [paymentUrl]); // Memastikan useEffect dijalankan ulang jika paymentUrl berubah
 
   return (
     <>
